0766954751a6918af6e651b3a99d7f67c3794036
felixeins <34221200+felixeins@users.noreply.github.com>
2017-12-21 14:22:48 +0100

    swag

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-function toggleClass(el) {
-  var boxes = document.getElementbyID('content').children;
-  for (var i = 0; i < boxes.length; i++){
-    boxes[i].className = "class1";
-  }
-  el.className = ""
-}
+function.toggleClass
